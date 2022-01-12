afbdcd265b3f94b5bd98331f951a3a0483916145
JoeSenna <joe.senna@live.com>
2022-01-11 21:59:22 -0300

    Criação das páginas

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Somador</title>
+</head>
+<body>
+    <form action="soma.php" method="POST">
+        <input type="number" name="num1" placeholder="primeiro número">
+        <input type="number" name="num2" placeholder="segundo número">
+        <input type="submit" value="Somar">
+    </form>
+</body>
+</html>
